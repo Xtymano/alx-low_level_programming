d9557ff027880be39620a55b29dfd4e41a377c9d
Xtymano <xtymano@gmail.com>
2023-04-21 21:01:15 -0700

    2-print_alpabet.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A program that prints the alphabet in lowercase, and then in uppercase
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	int n = 97;
+	
+	return (0);
+}
