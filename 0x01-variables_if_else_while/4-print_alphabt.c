144da9af515df28bd92531fc50414f0afd8c8eec
Xtymano <xtymano@gmail.com>
2023-04-21 22:49:04 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char n;
+
+	n = 'a';
+	while
+		(n <= 'z') {
+			if ((n != 'q' || n != 'e') && n <= 'z')
+				putchar(n);
+			n++;
+		}
+	putchar('\n');
+	return (0);
+}
