48647e68f7fef0b24313667a5e7c5b707cd5349b
Xtymano <xtymano@gmail.com>
2023-05-01 07:22:07 -0700

    100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,6 +3,7 @@
  *print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
  */
+
 void print_times_table(int n)
 {
 	int i, j, k;
@@ -35,18 +36,10 @@ void print_times_table(int n)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar((k / 10) + '0');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
 					_putchar((k % 10) + '0');
-				} else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0';
-					_putchar(((k / 10) % 10) + '0';
-					_putchar((k % 10) + '0');
-					}
 				}
-
 			}
 			_putchar('\n');
 		}
