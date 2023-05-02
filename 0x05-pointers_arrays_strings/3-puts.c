8983ac633f1b6a32651eedbe6a88d7755989cfa1
Xtymano <xtymano@gmail.com>
2023-05-02 05:47:25 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - a function that prints a string, followed by a new line, to stdout
+ * @str: prints a string
+ *
+ */
+
+void _puts(char *str)
+{
+	int middle;
+
+	for (middle = 0; str[middle] != '\0'; middle++)
+	{
+		_putchar(str[index]);
+	}
+	_putchar('\n');
+}
