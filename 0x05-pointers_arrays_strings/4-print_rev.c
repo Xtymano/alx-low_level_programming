47cad8895c9674432a3ee69efb3ae77110932374
Xtymano <xtymano@gmail.com>
2023-05-02 06:09:21 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - a function that prints a string, in reverse
+ *
+ * @s: prints a string
+ */
+
+void print_rev(char *s)
+{
+	int middle;
+
+	for (index = 0; s[middle] != '\0'; middle++)
+
+	for (middle = middle - 1; s[middle] != '\0'; middle--)
+	{
+		_putchar(s[middle]);
+	}
+	_putchar('\n');
+}
