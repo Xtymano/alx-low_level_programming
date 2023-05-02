f2386247f0f23e37c5158fac31402e0f2e358a00
Xtymano <xtymano@gmail.com>
2023-05-02 14:25:04 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,27 +3,22 @@
 
 /**
  * *_strcpy - a function that copies the string pointed to by src
- * @dest - the pointer to the buffer in which we copy the string
- * @src - This is the copied string
+ * @dest: This is destiny
+ * @src: This is the copied string
  *
- * Return: This is a pointer to dest 
+ * Return: This return copy
  */
 
 char *_strcpy(char *dest, char *src)
 {
-	int len, i;
+	char *start = dest;
 
-	len = 0;
-
-	while (src[len] != '\0')
+	while (*srec != '\0')
 	{
-		len++;
+		*dest = *src;
+		dest++
+		src++;
 	}
-	for (i = 0; i < len; i++)
-	{
-		dest[i] = src[i];
-	}
-	dest[i] = '\0';
-
-	return (dest);
+	*dest = '\0';
+	return (start);
 }
