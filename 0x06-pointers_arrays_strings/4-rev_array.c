531bfc27bfe9dead1d8b58749d0cf20551b926ce
Xtymano <xtymano@gmail.com>
2023-05-04 08:02:29 -0700

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reverse_array - prints reverse array.
+ * @a: array to be compared.
+ * @n: size of array.
+ * Return: reversed array.
+ */
+void reverse_array(int *a, int n)
+{
+	int change, start, stop;
+
+	start = 0;
+	stop = n - 1;
+	while (start < stop)
+	{
+		change = *(a + start);
+		*(a + start) = *(a + stop);
+		*(a + stop) = change;
+		satrt++;
+		stop--;
+	}
+}
