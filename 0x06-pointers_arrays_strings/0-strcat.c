1e3b043f18258a08f8e139e35da4d4187e02a384
Xtymano <xtymano@gmail.com>
2023-05-04 10:03:24 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,16 +9,15 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int w = -1, i;
+int a = -1, i;
+for (i = 0; dest[i] != '\0'; i++)
+;
 
-	for (i = 0; dest[i] != '\0'; i++)
-		;
+do {
+	a++;
+	dest[i] = src[a];
+	i++;
+} while (src[a] != '\0');
 
-	do {
-		w++;
-		dest[i] = src[w];
-		w++;
-	} while (src[w] != '\0');
-
-	return (dest);
+return (dest);
 }
