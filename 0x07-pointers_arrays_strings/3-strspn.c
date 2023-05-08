b684079f4c52b5712d5dec633d533bb80be4fef9
Xtymano <xtymano@gmail.com>
2023-05-08 14:34:38 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,18 +8,18 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int n = 0;
-	int y;
+	int r;
 
 	while (*s)
 	{
-		for (r = 0; accept[y]; y++)
+		for (r = 0; accept[r]; r++)
 		{
-			if (*s == accept[y])
+			if (*s == accept[r])
 			{
 				n++;
 				break;
 			}
-			else if (accept[y + 1] == '\0')
+			else if (accept[r + 1] == '\0')
 				return (n);
 		}
 		s++;
